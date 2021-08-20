a619a9aa244c0c9d1374d770cdf694d267393d14
kmtusher97 <kmtusher97@gmail.com>
2021-08-20 12:38:55 +0600

    Update init dev script

--- init-dev-script.sh
+++ init-dev-script.sh
@@ -2,18 +2,39 @@
 
 cd ..
 
-cp  daily-circular-dev/docker-compose.yml .
+cp daily-circular-dev/docker-compose.yml .
 
-echo '------------------------------'
-echo 'cloning project repos ....'
-echo '------------------------------'
+echo "------------------------------"
+echo "cloning project repos ...."
+echo "------------------------------"
 
-git clone git@github.com:Work-Station-101/daily-circular-client.git
+clone_project() {
+  echo -e "\ncloning $1 ...\n"
+  
+  if [ -d "$1" ]; then
+    echo -e "$1 already exits! cloning aborted!!!\n\n"
+    return -1
+  fi
+  
+  git clone "git@github.com:Work-Station-101/$1.git"
+  cd $1
+  git remote rename origin dc
+  cd ..
+  echo -e "\ncloning completed!\n\n"
 
-echo '------------------------------'
-echo 'cloning complete !!!'
-echo '------------------------------'
-echo 'start docker containers ...'
-echo '------------------------------'
+  return 1
+}
+
+projects[0]="daily-circular-client"
+
+for project in $projects; do
+  clone_project $project
+done
+
+echo -e "\n------------------------------"
+echo "cloning complete !!!"
+echo "------------------------------"
+echo "start docker containers ..."
+echo "------------------------------"
 
 docker-compose up -d --build
